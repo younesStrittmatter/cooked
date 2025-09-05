c919556aac36d0e24de9bdc72636844efd61081a
Younes Strittmatter <younes_strittmatter@brown.edu>
2025-09-05 10:15:16 -0400

    make spoiled broth main game

---
+++ analysis/bundle.py
@@ -0,0 +1,32 @@
+import os
+import pandas as pd
+
+def main():
+    df_all = pd.DataFrame()
+    df_all_with_history = pd.DataFrame()
+    for folder in os.listdir('./bundles'):
+        if folder == '.DS_Store' or folder == 'maps':
+            continue
+        for game_id in os.listdir(os.path.join('./bundles', folder)):
+            if game_id == '.DS_Store':
+                continue
+            # get the two csv files that end with _positions.csv
+            action_files = [f for f in os.listdir(os.path.join('./bundles', folder, game_id)) if f.endswith('_actions.csv')]
+            processed_files = [f for f in os.listdir(os.path.join('./bundles', folder, game_id)) if f.endswith('_processed.csv')]
+            for pf in action_files:
+                df = pd.read_csv(os.path.join('./bundles', folder, game_id, pf))
+                # add player
+                df_all = pd.concat([df_all, df])
+            for pf in processed_files:
+                df = pd.read_csv(os.path.join('./bundles', folder, game_id, pf))
+                # add player
+                df_all_with_history = pd.concat([df_all_with_history, df])
+
+    df_all.to_csv('./combined_actions.csv', index=False)
+    df_all_with_history.to_csv('./combined_actions_with_history.csv', index=False)
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ analysis/filter_replays.py
@@ -0,0 +1,57 @@
+import os
+import json
+
+REPLAY_FOLDER_RAW = './replays_raw/replays'
+REPLAY_FOLDER = './replays'
+
+EXPECTED_NR_OF_AGENTS = 2
+
+def main():
+    errors = {
+        'unexpected_agent_count': [],
+        'mismatched_game_id': [],
+        'no_intents': [],
+        'only_one_player_intent': [],
+        'invalid_json': []
+    }
+    for filename in os.listdir(REPLAY_FOLDER_RAW):
+        if filename.endswith('.json'):  # Process only JSON files
+            filepath_in = os.path.join(REPLAY_FOLDER_RAW, filename)
+            filepath_out = os.path.join(REPLAY_FOLDER, filename)
+            try:
+                json_data = json.load(open(filepath_in))
+            except json.JSONDecodeError:
+                errors['invalid_json'].append(filename)
+                continue
+
+            agents = json_data["agents"]
+            if not len(agents) == EXPECTED_NR_OF_AGENTS:
+                errors['unexpected_agent_count'].append(filename)
+                continue
+
+            game_ids = [el.get("initial_state").get('url_params').get('gameId') for el in agents.values()]
+            game_ids = [el[0] for el in game_ids if isinstance(el, list) and len(el) > 0]
+            if not len(set(game_ids)) <= 1:
+                errors['mismatched_game_id'].append(filename)
+                continue
+
+            intents = json_data["intents"]
+            if not isinstance(intents, list) or len(intents) <= 0:
+                errors['no_intents'].append(filename)
+                continue
+
+            player_intents = [intent['agent_id'] for intent in intents if intent.get('agent_id') in agents]
+            if len(set(player_intents)) < 2:
+                errors['only_one_player_intent'].append(filename)
+                continue
+
+            with open(filepath_out, 'w') as dat:
+                json.dump(json_data, dat, indent=4)
+
+
+    with open('filter_report.json', 'w') as rep:
+        json.dump(errors, rep, indent=4)
+
+
+if __name__ == '__main__':
+    main()
